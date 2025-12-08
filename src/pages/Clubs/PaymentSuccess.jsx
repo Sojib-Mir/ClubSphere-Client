708455b07a66066530f954b2a8903cd38fb6ee9e
SojibMir27 <mdsojibmir2027@gmail.com>
2025-12-09 00:57:01 +0600

    added club join payment system integration

--- src/pages/Clubs/PaymentSuccess.jsx
+++ src/pages/Clubs/PaymentSuccess.jsx
@@ -44,7 +44,7 @@ const PaymentSuccess = () => {
           </p>
           <Link
             to="/dashboard/payment-history"
-            className="inline-block bg-lime-500 text-white font-semibold py-2 px-4 rounded hover:bg-lime-600 transition duration-300"
+            className="inline-block bg-lime-500 text-white font-semibold py-2 px-4 rounded hover:bg-lime-600 transition duration-30"
           >
             Go to My Orders
           </Link>
